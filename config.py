f9305f2a411f02438718d17e38f1d99ea1fd4733
littlelovely <191861751@qq.com>
2019-07-02 07:32:08 +0800

    Merge branch 'master' of https://github.com/littlelovely/flasky
    # Conflicts:
    #	flasky.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config:
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
+    FLASKY_MAIL_SENDER = 'Brian Zhao <brian.zhao@live.cn>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
 
     @staticmethod
